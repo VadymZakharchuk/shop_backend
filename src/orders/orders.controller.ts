39844e6be58d4ac1bbe0c1ab131d48f7a0317727
vzakharchuk-pri <vadym.zakharchuk@proximaresearch.com>
2025-03-24 14:56:38 +0200

    add create Order endpoint

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -10,4 +10,28 @@ router.get('/',
     res.status(200).json(orders);
   }
 )
+
+router.get('/:id', async (req, res) => {
+  const orders = await ordersService.orderOne(req.params.id);
+  res.status(200).json(orders);
+})
+
+router.post('/', async (req, res) => {
+  const order = await ordersService.createOrder(req.body);
+  res.status(201).json(order);
+})
+
+router.put('/:id', async (req, res) => {
+  const orders = await ordersService.updateOne(req.params.id, req.body);
+  res.status(200).json(orders);
+})
+
+router.delete('/:id', async (req, res) => {
+  const orders = await ordersService.deleteOne(req.params.id);
+  res.status(200).json(orders);
+})
+
+router.put('/status/:id', async (req, res) => {
+
+})
 export const ordersRouter = router;
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -2,6 +2,9 @@ import { prisma } from "@/server";
 import { logger } from "@/utils/log";
 import { Orders } from "@prisma/client";
 
+type nextOrderNo = {
+  f0: string
+}
 export class OrdersService {
   async ordersList(query: any): Promise<Orders[]> {
     try {
@@ -21,11 +24,11 @@ export class OrdersService {
     }
   }
 
-  async ordersOne(id: string) {
+  async orderOne(id: string) {
     try {
       return await prisma.orders.findMany({
         where: {
-          id: +id
+          orderNo: id
         },
         include: {
           Users: true,
@@ -39,19 +42,52 @@ export class OrdersService {
     }
   }
 
-  async createOne(ordersItem: Orders) {
+  async createOrder(orderItems: Orders[]) {
     try {
-      return await prisma.orders.create({ data: ordersItem });
+      // get next order number
+      const { _max } = await prisma.orders.aggregate({ _max: { orderNo: true } })
+      const orderNo:  string | null = _max.orderNo ? (+_max.orderNo + 1).toString() : null;
+      console.log(orderNo);
+
+      // check if transaction available
+      let isTransactionAvailable =  true
+      const result = []
+      for (const item of orderItems) {
+        const stock = await prisma.stock.findUnique({ where: { id: item.stockRef } })
+        if (!stock || !item.quantity || stock.countStock < item.quantity) {
+          isTransactionAvailable = false
+          result.push(`not enough stock for stock ID :: ${item.stockRef}`)
+        }
+        item.orderNo = orderNo
+        item.operDate = new Date();
+      }
+
+      if (!isTransactionAvailable) { return result }
+
+      for (const item of orderItems) {
+        await prisma.stock.update({
+          where: { id: item.stockRef },
+          data: {
+            countStock: {
+              decrement: item.quantity!
+            },
+            countOrdered: {
+              increment: item.quantity!
+            }
+          }
+        })
+      }
+      return await prisma.orders.createMany({ data: [...orderItems] })
     }
     catch (e) {
       logger.error(e);
-      throw new Error('createOne service error');
+      throw new Error(`insertInto service error ${e}`);
     }
   }
 
-  async updateOne(id: string, ordersItem: Orders) {
+  async updateOne(id: string, orderItem: Orders) {
     try {
-      return await prisma.orders.update({ where: { id: +id }, data: ordersItem });
+      return await prisma.orders.update({ where: { id: +id }, data: orderItem });
     }
     catch (e) {
       logger.error(e);
@@ -62,6 +98,16 @@ export class OrdersService {
   async deleteOne(id: string) {
     try {
       return await prisma.orders.delete({ where: { id: +id } });
+    }
+    catch (e) {
+      logger.error(e);
+      throw new Error('deleteOne service error');
+    }
+  }
+
+  async updateOrderStatus(orderId: string, orderStatus: string, direction = true) {
+    try {
+      return await prisma.$executeRaw`call updateOrderStockStatus(${orderId}, ${orderStatus}, ${direction})`;
     }
     catch (e) {
       logger.error(e);
